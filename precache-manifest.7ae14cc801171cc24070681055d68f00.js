88c567779fe38eb589f6484858ed2859df633020
Ксения <ksenya.zaharova92@gmail.com>
2020-05-18 21:02:19 +0300

    Updates

---
+++ precache-manifest.7ae14cc801171cc24070681055d68f00.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660cfa4887467ecc57051b98ebcdb418",
+    "url": "/react_shop/index.html"
+  },
+  {
+    "revision": "9c518fc44d29d3ac067d",
+    "url": "/react_shop/static/css/main.56019022.chunk.css"
+  },
+  {
+    "revision": "c8ef472f827751afd8a0",
+    "url": "/react_shop/static/js/2.2a7119de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_shop/static/js/2.2a7119de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c518fc44d29d3ac067d",
+    "url": "/react_shop/static/js/main.fdc8c988.chunk.js"
+  },
+  {
+    "revision": "ad1d48ed5d19a072ab03",
+    "url": "/react_shop/static/js/runtime-main.daa74fa5.js"
+  },
+  {
+    "revision": "03d8e9cc564f50de2945fcae7273dd0b",
+    "url": "/react_shop/static/media/butter.03d8e9cc.png"
+  },
+  {
+    "revision": "dc01bc73c0e3916373dce4fac568709f",
+    "url": "/react_shop/static/media/cake.dc01bc73.png"
+  },
+  {
+    "revision": "b5bd6b8c6bf5d346a93b48be8a9f2a5b",
+    "url": "/react_shop/static/media/cheese.b5bd6b8c.png"
+  },
+  {
+    "revision": "2ab35258256f98bb4bac84bba5c0723a",
+    "url": "/react_shop/static/media/common-pik-food.2ab35258.png"
+  },
+  {
+    "revision": "c5c22dcdd725f053a0dbca265bcec4ca",
+    "url": "/react_shop/static/media/crisps.c5c22dcd.png"
+  },
+  {
+    "revision": "7b52aae15c48d0eb524cde4e0724292f",
+    "url": "/react_shop/static/media/cup.7b52aae1.png"
+  },
+  {
+    "revision": "0ee52fb60096dfecb00e77637a54d84f",
+    "url": "/react_shop/static/media/fork.0ee52fb6.png"
+  },
+  {
+    "revision": "48fb7d1eb37b248399cec2db43f0a345",
+    "url": "/react_shop/static/media/fridge.48fb7d1e.png"
+  },
+  {
+    "revision": "e7e790a00c87b5c68a98dcfe91833a7d",
+    "url": "/react_shop/static/media/hairdryer.e7e790a0.png"
+  },
+  {
+    "revision": "ae28ed3140b5bcf94e6a0c3edef7c53c",
+    "url": "/react_shop/static/media/iron.ae28ed31.png"
+  },
+  {
+    "revision": "21bc74071330241840d354105a73089a",
+    "url": "/react_shop/static/media/kettle.21bc7407.png"
+  },
+  {
+    "revision": "6ecb1e01e1aec66644f7aa60349a10c1",
+    "url": "/react_shop/static/media/knife.6ecb1e01.png"
+  },
+  {
+    "revision": "5d93acdd2585ef70714ac3c203767b7f",
+    "url": "/react_shop/static/media/mikser.5d93acdd.png"
+  },
+  {
+    "revision": "7b51d29f272d94957874e00b80d044fc",
+    "url": "/react_shop/static/media/pasta.7b51d29f.png"
+  },
+  {
+    "revision": "d5eb3345eb8e59a4e97eb935d1eb8028",
+    "url": "/react_shop/static/media/plate.d5eb3345.png"
+  },
+  {
+    "revision": "a6d6485d94d9c64be15ad5c4f9555bf5",
+    "url": "/react_shop/static/media/spoon.a6d6485d.png"
+  }
+]);
